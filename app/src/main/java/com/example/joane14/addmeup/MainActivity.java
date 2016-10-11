1bf7c85242434e16c1310a25dd96025d06006236
Joane Therese Artiaga <Joane Therese Artiaga>
2016-10-12 07:57:34 +0800

    First commit

--- app/src/main/java/com/example/joane14/addmeup/MainActivity.java
+++ app/src/main/java/com/example/joane14/addmeup/MainActivity.java
@@ -4,16 +4,30 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.btnResult);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMe(v);
+            }
+        });
+
+
     }
+
 
 
     public void addMe(View view){
@@ -39,5 +53,16 @@ public class MainActivity extends AppCompatActivity {
             alertDialog.show();
         }
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnResult:
+                
+                break;
+            default:
+                break;
+        }
     }
 }
